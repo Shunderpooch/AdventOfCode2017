38af7d324d58c5e5ff8d7cae41547fd4da7bb200
Arthur Dooner <shunderpooch@gmail.com>
2017-12-05 10:08:28 -0500

    Finished Part 1 of Day 5

---
+++ Day 5/Maze_of_Twisty_Trampolines_1.py
@@ -0,0 +1,19 @@
+import sys
+
+JUMP_OFFSETS = []
+
+with open("maze.txt", "r") as filestream:
+    for line in filestream:
+        JUMP_OFFSETS.append(int(line)) #Convert to int as we go
+
+sys.stdout.flush()
+
+CURRENT_INDEX = 0
+NUM_STEPS = 0
+while CURRENT_INDEX >= 0 and CURRENT_INDEX < len(JUMP_OFFSETS):
+    PREV_INDEX = CURRENT_INDEX
+    CURRENT_INDEX += JUMP_OFFSETS[CURRENT_INDEX]
+    JUMP_OFFSETS[PREV_INDEX] += 1
+    NUM_STEPS += 1
+
+print(f'The number of steps to exit the maze are {NUM_STEPS}')
