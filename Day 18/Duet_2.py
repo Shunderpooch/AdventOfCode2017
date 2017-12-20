e6dbd7d6c4cf7f55d7dd1516cfa0ea4066567b4d
Arthur Dooner <shunderpooch@gmail.com>
2017-12-20 10:13:44 -0500

    Finished Part 2 of Day 18

--- Day 18/Duet_2.py
+++ Day 18/Duet_2.py
@@ -1,9 +1,8 @@
 # Author Arthur Dooner
 # Advent of Code Day 18
-# Part 1
+# Part 2
 
-INSTRUCTIONS = []
-LAST_PLAYED_SOUND = None
+from multiprocessing import Process, Pipe, Lock
 
 def get_value(register_or_num, register_dict):
     if register_or_num.isalpha():
@@ -13,15 +12,16 @@ def get_value(register_or_num, register_dict):
     else:
         return int(register_or_num)
 
-def run_instruction(register_dict, an_instruction):
-    global LAST_PLAYED_SOUND
+def run_instruction(register_dict, an_instruction, a_pipe, program_counter):
     if len(an_instruction) == 2:
         if an_instruction[0] == "snd":
-            LAST_PLAYED_SOUND = get_value(an_instruction[1], register_dict)
+            a_pipe.send(get_value(an_instruction[1], register_dict))
+            program_counter += 1
         elif an_instruction[0] == "rcv":
-            if get_value(an_instruction[1], register_dict) != 0:
-                print(f'The first value of the recovered frequency (Part 1) is {LAST_PLAYED_SOUND}')
-                exit(0)
+            if a_pipe.poll(1):
+                register_dict[an_instruction[1]] = int(a_pipe.recv())
+            else:
+                raise EOFError("Done with the program")
         else:
             raise Exception("An unrecognized command was given, and the program will exit.")
             exit(0)
@@ -30,7 +30,7 @@ def run_instruction(register_dict, an_instruction):
             register_dict[an_instruction[1]] = get_value(an_instruction[1], register_dict) + get_value(an_instruction[2], register_dict)
         elif an_instruction[0] == "jgz":
             if get_value(an_instruction[1], register_dict) > 0:
-                return get_value(an_instruction[2], register_dict)
+                return get_value(an_instruction[2], register_dict), program_counter
         elif an_instruction[0] == "mod":
             register_dict[an_instruction[1]] = get_value(an_instruction[1], register_dict) % get_value(an_instruction[2], register_dict)
         elif an_instruction[0] == "mul":
@@ -40,19 +40,35 @@ def run_instruction(register_dict, an_instruction):
         else:
             raise Exception("An unrecognized command was given, and the program will exit.")
             exit(0)
-    return 1
+    return 1, program_counter
 
-with open("Instructions.txt", "r") as instructions:
-    for line in instructions:
-        an_instruction = line.split()
-        INSTRUCTIONS.append(an_instruction)
-
-def run_instructions(all_instructions):
+def run_instructions(all_instructions, p_state, a_pipe, program_counter):
     instruction_index = 0
     instruction_change = 0
     registers = {}
+    registers['p'] = p_state
     while instruction_index < len(all_instructions):
-        instruction_change = run_instruction(registers, all_instructions[instruction_index])
+        try:
+            instruction_change, program_counter = run_instruction(registers, all_instructions[instruction_index], a_pipe, program_counter)
+        except EOFError as identifier:
+            if (p_state == 1):
+                print(f'Program {p_state} (Part 2) sent {program_counter} programs.')
+            exit(0)
         instruction_index += instruction_change
 
-run_instructions(INSTRUCTIONS)
+if __name__ == "__main__":
+    INSTRUCTIONS = []
+    with open("Instructions.txt", "r") as instructions:
+        for line in instructions:
+            an_instruction = line.split()
+            INSTRUCTIONS.append(an_instruction)
+    connection_left, connection_right = Pipe()
+    Program0Counter = 0
+    Program1Counter = 0
+    program_0 = Process(target=run_instructions, args=(INSTRUCTIONS, 0, connection_left, Program0Counter))
+    program_1 = Process(target=run_instructions, args=(INSTRUCTIONS, 1, connection_right, Program1Counter))
+    program_0.start()
+    program_1.start()
+    program_0.join()
+    program_1.join()
+        
